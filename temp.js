1cde5a02b9957849033ea1ffed6f51417d9e0505
gautam <gautamguddu577@gmail.com>
2025-03-04 08:58:42 +0530

    r

---
+++ temp.js
@@ -0,0 +1,41 @@
+
+// let num =  () => {
+
+//     return new Promise( (resolve, reject) => {
+//         resolve(25);
+//     } )
+    
+// }
+
+// let num2 = () => 25;
+
+// // console.log(num);
+
+// async function getNumber() {
+
+//     let data = await 25;
+//     console.log(data);
+//     console.log("inside fn");
+    
+// }
+
+// getNumber();
+
+// console.log("outside");
+
+
+
+
+console.log(printName());
+
+console.log(a);
+
+
+var a = 100;
+
+
+var printName = (name) => {
+
+console.log(name)
+
+}
