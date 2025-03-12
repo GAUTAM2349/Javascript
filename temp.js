74d29da27edac9fa3043e0191166c1ebaa1b2487
gautam <gautamguddu577@gmail.com>
2025-03-13 01:29:46 +0530

    javascript 13-march-2025

--- temp.js
+++ temp.js
@@ -1,41 +1,21 @@
+async function foo() {
 
-// let num =  () => {
-
-//     return new Promise( (resolve, reject) => {
-//         resolve(25);
-//     } )
-    
-// }
-
-// let num2 = () => 25;
-
-// // console.log(num);
-
-// async function getNumber() {
-
-//     let data = await 25;
-//     console.log(data);
-//     console.log("inside fn");
-    
-// }
-
-// getNumber();
-
-// console.log("outside");
-
-
-
-
-console.log(printName());
-
-console.log(a);
-
-
-var a = 100;
-
-
-var printName = (name) => {
-
-console.log(name)
-
-}
+    console.log("A");
+  
+    await Promise.resolve();
+  
+    console.log("B");
+  
+    await new Promise(resolve => setTimeout(resolve, 0));
+  
+    console.log("C");
+  
+  }
+  
+  
+  
+  console.log("D");
+  
+  foo();
+  
+  console.log("E");
